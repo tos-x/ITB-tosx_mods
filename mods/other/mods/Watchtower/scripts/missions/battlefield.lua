78acc21273b3d87e092e9ff4d94d99de8a91be98
tos-x <mkbish@gmail.com>
2023-04-14 09:03:31 -0400

    Fix battlefield mission so starting Vek are properly wounded

--- mods/other/mods/Watchtower/scripts/missions/battlefield.lua
+++ mods/other/mods/Watchtower/scripts/missions/battlefield.lua
@@ -17,6 +17,22 @@ for personalityId, dialogTable in pairs(dialog) do
 	Personality[personalityId]:AddMissionDialogTable("Mission_tosx_Battlefield", dialogTable)
 end
 
+-- PawnTrackedHook doesn't seem to catch starting spawns
+-- Neither does StartMission()
+function Mission_tosx_Battlefield:UpdateMission()
+	if Game:GetTurnCount() == 0 and Game:GetTeamTurn() == TEAM_ENEMY then
+		local foes = extract_table(Board:GetPawns(TEAM_ENEMY))
+		for i,id in pairs(foes) do
+			local pawn = Board:GetPawn(id)
+			local hp = math.max(1,pawn:GetHealth() - 2)
+			if pawn:GetHealth() > hp then
+				--LOG("UPDATE Setting pawn ",pawn:GetId()," (type: ",pawn:GetType(),") to hp: ",hp)
+				pawn:SetHealth(hp)
+			end
+		end
+	end
+end
+
 -- Have to do this via a hook, because adjusting the spawn functions won't save the
 -- pawn:SetHealth() change to spawns if game is quit and restarted
 local function onTracked(mission, pawn)
