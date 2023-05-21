be94fe36228053bff1ad3719613e35a24f3f9451
tos-x <mkbish@gmail.com>
2023-05-20 20:04:29 -0400

    Update to 1.20

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,7 +5,7 @@ return {
 	description = "Mods by tosx.",
 	icon = "scripts/icon.png",
 	submodFolders = {"mods/"},
-	version = "1.19",
+	version = "1.20",
 	init = function() end,
 	load = function() end
 }
