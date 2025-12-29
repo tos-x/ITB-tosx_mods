bd95e5f6c61f1d34badf8d456b44bc0e910f0682
tos-x <mkbish@gmail.com>
2025-12-29 09:07:09 -0500

    Update mod versions

--- mods/other/mods/Watchtower/scripts/init.lua
+++ mods/other/mods/Watchtower/scripts/init.lua
@@ -1,7 +1,7 @@
 local mod = {
 	id = "tosx_watchtower",
 	name = "Watchtower Security",
-	version = "0.0.5",
+	version = "0.0.6",
 	modApiVersion = "2.9.2",
 	icon = "img/icon.png",
 	requirements = {"tosx_island_missons"},
--- scripts/init.lua
+++ scripts/init.lua
@@ -5,7 +5,7 @@ return {
 	description = "Mods by tosx.",
 	icon = "scripts/icon.png",
 	submodFolders = {"mods/"},
-	version = "1.27",
+	version = "1.28",
 	init = function() end,
 	load = function() end
 }
