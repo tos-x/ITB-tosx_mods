da7d66c86fb201d076c00fc0368a775433b0c74a
tos-x <mkbish@gmail.com>
2026-01-04 21:51:08 -0500

    Update version numbers

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,7 +5,7 @@ return {
 	description = "Mods by tosx.",
 	icon = "scripts/icon.png",
 	submodFolders = {"mods/"},
-	version = "1.29",
+	version = "1.30",
 	init = function() end,
 	load = function() end
 }
